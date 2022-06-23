22b9ef981c3ccacc6bfc79b4dc69e3aaa9c3f733
acbca <idacbca@126.com>
2022-06-23 03:15:15 +0000

    Renamed models, added database schema

--- service/models.py
+++ service/models.py
@@ -1,9 +1,12 @@
 """
-Models for YourResourceModel
+Models for CustomersModel
 
 All of the models are stored in this module
 """
 import logging
+from enum import Enum
+from datetime import date
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -17,63 +20,87 @@ class DataValidationError(Exception):
 
     pass
 
+class Gender(Enum):
+    """Enumeration of valid Pet Genders"""
 
-class YourResourceModel(db.Model):
+    MALE = 0
+    FEMALE = 1
+    UNKNOWN = 3
+
+class CustomersModel(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a CustomersModel
     """
 
     app = None
 
     # Table Schema
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    customer_id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(63))
+    first_name = db.Column(db.String(63))
+    last_name = db.Column(db.String(63))
+    nickname = db.Column(db.String(63))
+    email = db.Column(db.String(63))
+    gender = db.Column(
+        db.Enum(Gender), nullable=False, server_default=(Gender.UNKNOWN.name)
+    )
+    birthday = db.Column(db.Date(), nullable=False, default=date.today())
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return "<CustomersModel %r customer_id=[%s]>" % (self.first_name, self.customer_id)
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a CustomersModel to the database
         """
-        logger.info("Creating %s", self.name)
-        self.id = None  # id must be none to generate next primary key
+        logger.info("Creating %s", self.first_name)
+        self.customer_id = None  # customer_id must be none to generate next primary key
         db.session.add(self)
         db.session.commit()
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a CustomersModel to the database
         """
-        logger.info("Saving %s", self.name)
+        logger.info("Saving %s", self.first_name)
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
-        logger.info("Deleting %s", self.name)
+        """ Removes a CustomersModel from the data store """
+        logger.info("Deleting %s", self.first_name)
         db.session.delete(self)
         db.session.commit()
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        """ Serializes a CustomersModel into a dictionary """
+        return {"customer_id": self.customer_id, "first_name": self.first_name, "last_name": self.last_name, "nickname": self.nickname, 
+        "email": self.email, "gender": self.gender, "birthday": self.birthday}
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a CustomersModel from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.first_name = data["first_name"]
+            self.last_name = data["last_name"]
+            self.nickname = data["nickname"]
+            self.email = data["email"]
+            self.gender = getattr(Gender, data["gender"])  # create enum from string
+            self.birthday = date.fromisoformat(data["birthday"])
+        except AttributeError as error:
+            raise DataValidationError(
+                "Invalid attribute: " + error.args[0]
+            )
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
+                "Invalid CustomersModel: missing " + error.args[0]
             )
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data"
+                "Invalid CustomersModel: body of request contained bad or no data"
             )
         return self
 
@@ -89,22 +116,120 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+        """ Returns all of the CustomersModels in the database """
+        logger.info("Processing all CustomersModels")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
+        """ Finds a CustomersModel by it's customer_id """
+        logger.info("Processing lookup for customer_id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
-    def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+    def find_by_name(cls, first_name):
+        """Returns all CustomersModels with the given first_name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            first_name (string): the first_name of the CustomersModels you want to match
         """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name)
+        logger.info("Processing first_name query for %s ...", first_name)
+        return cls.query.filter(cls.first_name == first_name)
+
+
+
+class AddressesModel(db.Model):
+    """
+    Class that represents a AddressesModel
+    """
+
+    app = None
+
+    # Table Schema
+    customer_id = db.Column(db.Integer, primary_key=True)
+    address_id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(63))
+
+    def __repr__(self):
+        return "<AddressesModel %r customer_id=[%s] address_id=[%s]>" % (self.first_name, self.customer_id, self.address_id)
+
+    def create(self):
+        """
+        Creates a AddressesModel to the database
+        """
+        logger.info("Creating %s", self.first_name)
+        self.customer_id = None  # customer_id must be none to generate next primary key
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates a AddressesModel to the database
+        """
+        logger.info("Saving %s", self.first_name)
+        db.session.commit()
+
+    def delete(self):
+        """ Removes a AddressesModel from the data store """
+        logger.info("Deleting %s", self.first_name)
+        db.session.delete(self)
+        db.session.commit()
+
+    def serialize(self):
+        """ Serializes a AddressesModel into a dictionary """
+        return {"customer_id": self.customer_id, "address_id": self.address_id, "address": self.address}
+
+    def deserialize(self, data):
+        """
+        Deserializes a AddressesModel from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.address = data["address"]
+        except AttributeError as error:
+            raise DataValidationError(
+                "Invalid attribute: " + error.args[0]
+            )
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid AddressesModel: missing " + error.args[0]
+            )
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid AddressesModel: body of request contained bad or no data"
+            )
+        return self
+
+    @classmethod
+    def init_db(cls, app):
+        """ Initializes the database session """
+        logger.info("Initializing database")
+        cls.app = app
+        # This is where we initialize SQLAlchemy from the Flask app
+        db.init_app(app)
+        app.app_context().push()
+        db.create_all()  # make our sqlalchemy tables
+
+    @classmethod
+    def all(cls):
+        """ Returns all of the AddressesModels in the database """
+        logger.info("Processing all AddressesModels")
+        return cls.query.all()
+
+    @classmethod
+    def find(cls, by_id):
+        """ Finds a AddressesModel by it's customer_id """
+        logger.info("Processing lookup for customer_id %s ...", by_id)
+        return cls.query.get(by_id)
+
+    @classmethod
+    def find_by_name(cls, first_name):
+        """Returns all AddressesModels with the given first_name
+
+        Args:
+            first_name (string): the first_name of the AddressesModels you want to match
+        """
+        logger.info("Processing first_name query for %s ...", first_name)
+        return cls.query.filter(cls.first_name == first_name)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,35 +1,47 @@
 """
-Test cases for YourResourceModel Model
+Test cases for CustomersModel Model
 
 """
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
+from datetime import date
+from service.models import CustomersModel, DataValidationError, db
+from service import app
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@postgres:5432/postgres"
+)
 
 ######################################################################
 #  <your resource name>   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+# pylint: disable=too-many-public-methods
+class TestCustomersModel(unittest.TestCase):
+    """ Test Cases for CustomersModel Model """
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        CustomersModel.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        """This runs once after the entire test suite"""
+        db.session.close()
 
     def setUp(self):
-        """ This runs before each test """
-        pass
+        """This runs before each test"""
+        db.session.query(CustomersModel).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
-        pass
+        """This runs after each test"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S

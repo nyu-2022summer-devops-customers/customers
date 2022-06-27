fc81b3d7942ad9c6dd09042c0ae072f63e449b4c
ShereenLiao <86658013+ShereenLiao@users.noreply.github.com>
2022-06-27 18:16:23 -0400

    Revert "feat:update address"

--- service/models.py
+++ service/models.py
@@ -267,9 +267,18 @@ class AddressModel(db.Model):
         logger.info("Processing customer_id and address_id query for %s %s ...", customer_id, address_id)
         return cls.query.filter(cls.customer_id == customer_id).filter(cls.address_id == address_id)
     
+
+    @classmethod
+    def find_by_address_id(cls,  address_id):
+        """Get an Address information under address_id
+        Args:
+            address_id(int): address_id of the AddressModels you want to match
+        """
+        logger.info("Processing address_id query for %s ...",  address_id)
+        return cls.query.filter(cls.address_id == address_id)
     
     @classmethod
-    def update_address_by_address_and_customer_id(cls,address_id,customer_id,new_address):
+    def update_address_by_address_id(cls,address_id,new_address):
         """Update an Address information under address_id
         
         Args:
@@ -277,7 +286,7 @@ class AddressModel(db.Model):
         """
         logger.info("Processing address update for %s ...",  address_id)
         
-        address_found=AddressModel.find_by_customer_and_address_id(address_id,customer_id)
+        address_found=AddressModel.find_by_address_id(address_id)
         if address_found.count()==0:
             raise DataValidationError("the address_id dosen't exist")
         else:

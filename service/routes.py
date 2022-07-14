48a65cb5d798851ae9cf031d5288cc18bf5da30c
JunruHe <jh7948@nyu.edu>
2022-07-14 11:45:29 -0400

    fix F401 imported but unused

--- service/routes.py
+++ service/routes.py
@@ -4,22 +4,18 @@ My Service
 Describe what your service does here
 """
 
-from audioop import add
-import os
-import sys
-import logging
 from flask import jsonify, request, url_for, abort
 from .utils import status  # HTTP Status Codes
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
-from flask_sqlalchemy import SQLAlchemy
-from service.models import CustomerModel, AddressModel, DataValidationError
+from service.models import CustomerModel, AddressModel
 
 # Import Flask application
 from . import app
 
 BASE_URL = "/customers"
+
 
 ######################################################################
 # GET INDEX

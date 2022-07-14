24b6e8a3f2a49427d42df252706dd3482fff2cf0
JunruHe <jh7948@nyu.edu>
2022-07-14 12:03:18 -0400

    E303 too mant blank lines

--- service/routes.py
+++ service/routes.py
@@ -230,7 +230,7 @@ def delete_an_address_of_a_customer(customer_id, address_id):
 
     return "", status.HTTP_204_NO_CONTENT
 
-     
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################

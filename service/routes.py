811c5c03828571819e22feaa5f481059b7fe1d20
ufolux <richadlu001@gmail.com>
2022-07-19 01:58:14 +0000

    fix: remove activate customer params

--- service/routes.py
+++ service/routes.py
@@ -218,18 +218,19 @@ def list_customers():  # noqa: C901
 def activate_a_customers(customer_id):
     """Activate a customer"""
     app.logger.info("Request to activate customer with id: %s', customer_id")
-    check_content_type("application/json")
+    # check_content_type("application/json")
 
     customer = CustomerModel.find(customer_id)
     if not customer:
         abort(status.HTTP_404_NOT_FOUND, f"Customer with id '{customer_id}' was not found.")
 
-    customer.deserialize(request.get_json())
-    customer.customer_id = customer_id
     customer.is_active = True
     customer.update()
 
-    app.logger.info("Customer with ID [%s] updated.", customer.customer_id)
+    app.logger.info(
+        "Customer with ID [%s] updated.",
+        customer.customer_id
+    )
     return jsonify(customer.serialize()), status.HTTP_200_OK
 
 

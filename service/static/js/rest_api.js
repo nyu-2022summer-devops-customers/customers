0397e87c4ec881fe890f0eac469a558a40780ee9
acbca <idacbca@126.com>
2022-07-31 01:46:28 +0000

    fix: Updated index.html and rest_api.js - Commented out the retrieve button - Added password in result

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -7,14 +7,14 @@ $(function () {
     // Updates the form with data from the response
     function update_form_data(res) {
         $("#customer_id").val(res.customer_id);
-        $("#customer_first_name").val(res.customer_first_name);
-        $("#customer_last_name").val(res.customer_last_name);
-        $("#customer_nickname").val(res.customer_nickname);
-        $("#customer_password").val(res.customer_password);
-        $("#customer_email").val(res.customer_email);
-        $("#customer_gender").val(res.customer_gender);
-        $("#customer_birthday").val(res.customer_birthday);
-        if (res.customer_is_active == true) {
+        $("#customer_first_name").val(res.first_name);
+        $("#customer_last_name").val(res.last_name);
+        $("#customer_nickname").val(res.nickname);
+        $("#customer_password").val(res.password);
+        $("#customer_email").val(res.email);
+        $("#customer_gender").val(res.gender);
+        $("#customer_birthday").val(res.birthday);
+        if (res.is_active == true) {
             $("#customer_is_active").val("true");
         } else {
             $("#customer_is_active").val("false");
@@ -201,79 +201,81 @@ $(function () {
     // Search for a Customer
     // ****************************************
 
-    // $("#search-btn").click(function () {
+    $("#search-btn").click(function () {
 
-    //     let first_name = $("#first_name").val();
-    //     let last_name = $("#last_name").val();
-    //     let nickname = $("#nickname").val();
-    //     let birthday = $("#birthday").val();
-    //     let email = $("#email").val();
+        // let first_name = $("#first_name").val();
+        // let last_name = $("#last_name").val();
+        // let nickname = $("#nickname").val();
+        // let birthday = $("#birthday").val();
+        // let email = $("#email").val();
 
-    //     let queryString = ""
+        let queryString = ""
 
-    //     if (first_name && last_name) {
-    //         queryString += 'first name=' + first_name + 'last name=' + last_name
-    //     }
-    //     if (nickname) {
-    //         queryString += 'nick name=' + nickname
-    //     }
-    //     if (email) {
-    //         queryString += 'email=' + email
-    //     }
-    //     if (birthday) {
-    //         if (queryString.length > 0) {
-    //             queryString += '&birthday=' + birthday
-    //         } else {
-    //             queryString += 'birthday=' + birthday
-    //         }
-    //     }
+        // if (first_name && last_name) {
+        //     queryString += 'first name=' + first_name + 'last name=' + last_name
+        // }
+        // if (nickname) {
+        //     queryString += 'nick name=' + nickname
+        // }
+        // if (email) {
+        //     queryString += 'email=' + email
+        // }
+        // if (birthday) {
+        //     if (queryString.length > 0) {
+        //         queryString += '&birthday=' + birthday
+        //     } else {
+        //         queryString += 'birthday=' + birthday
+        //     }
+        // }
 
-    //     $("#flash_message").empty();
+        $("#flash_message").empty();
 
-    //     let ajax = $.ajax({
-    //         type: "GET",
-    //         url: `/customers?${queryString}`,
-    //         contentType: "application/json",
-    //         data: ''
-    //     })
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/customers?${queryString}`,
+            contentType: "application/json",
+            data: ''
+        })
 
-    //     ajax.done(function(res){
-    //         //alert(res.toSource())
-    //         $("#search_results").empty();
-    //         let table = '<table class="table table-striped" cellpadding="10">'
-    //         table += '<thead><tr>'
-    //         table += '<th class="col-md-2">ID</th>'
-    //         table += '<th class="col-md-2">First Name</th>'
-    //         table += '<th class="col-md-2">Last Name</th>'
-    //         table += '<th class="col-md-2">Nickname</th>'
-    //         table += '<th class="col-md-2">Gender</th>'
-    //         table += '<th class="col-md-2">Email</th>'
-    //         table += '<th class="col-md-2">Birthday</th>'
-    //         table += '<th class="col-md-2">Active</th>'
-    //         table += '</tr></thead><tbody>'
-    //         let firstCustomer = "";
-    //         for(let i = 0; i < res.length; i++) {
-    //             let customer = res[i];
-    //             table +=  `<tr id="row_${i}"><td>${customer.customer_id}</td><td>${customer.first_name}</td><td>${customer.last_name}</td><td>${customer.nickname}</td><td>${pet.gender}</td><td>${pet.email}</td><td>${pet.birthday}</td><td>${customer.is_active}</td></tr>`;
-    //             if (i == 0) {
-    //                 firstCustomer = customer;
-    //             }
-    //         }
-    //         table += '</tbody></table>';
-    //         $("#search_results").append(table);
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-1">ID</th>'
+            table += '<th class="col-md-2">First Name</th>'
+            table += '<th class="col-md-2">Last Name</th>'
+            table += '<th class="col-md-2">Nickname</th>'
+            table += '<th class="col-md-3">Password</th>'
+            table += '<th class="col-md-1">Gender</th>'
+            table += '<th class="col-md-2">Email</th>'
+            table += '<th class="col-md-2">Birthday</th>'
+            table += '<th class="col-md-2">IS Active</th>'
+            table += '</tr></thead><tbody>'
+            let firstCustomer = "";
+            for(let i = 0; i < res.length; i++) {
+                let customer = res[i];
+                table +=  `<tr id="row_${i}"><td>${customer.customer_id}</td><td>${customer.first_name}</td><td>${customer.last_name}</td><td>${customer.nickname}</td>
+                    <td>${customer.password}</td><td>${customer.gender}</td><td>${customer.email}</td><td>${customer.birthday}</td><td>${customer.is_active}</td></tr>`;
+                if (i == 0) {
+                    firstCustomer = customer;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
 
-    //         // copy the first result to the form
-    //         if (firstCustomer != "") {
-    //             update_form_data(firstCustomer)
-    //         }
+            // copy the first result to the form
+            if (firstCustomer != "") {
+                update_form_data(firstCustomer)
+            }
 
-    //         flash_message("Success")
-    //     });
+            flash_message("Success")
+        });
 
-    //     ajax.fail(function(res){
-    //         flash_message(res.responseJSON.message)
-    //     });
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
 
-    // });
+    });
 
 })

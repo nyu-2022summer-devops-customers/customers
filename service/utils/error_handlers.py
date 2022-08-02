b1487275166d98af41917a0eb6d1831d4371c9f5
ShereenLiao <86658013+ShereenLiao@users.noreply.github.com>
2022-08-01 22:29:48 -0400

    Update error_handlers.py

--- service/utils/error_handlers.py
+++ service/utils/error_handlers.py
@@ -36,7 +36,7 @@ def request_validation_error(error):
 @app.errorhandler(status.HTTP_400_BAD_REQUEST)
 def bad_request(error):
     """Handles bad requests with 400_BAD_REQUEST"""
-    message = str(error);
+    message = str(error)
     app.logger.warning(message)
     return (
         jsonify(
